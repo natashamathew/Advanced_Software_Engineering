8c418a746c988bd464a38247158e88a5b16e0e7d
bassidy10 <44612779+bassidy10@users.noreply.github.com>
2018-11-06 18:59:51 -0600

    Update html/Main/foodLog1.php
    Food Logging File

---
+++ html/Main/foodLog1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Smart </title>
+</head>
+
+<body>
+    <h1><strong> FOOD LOGGING </strong> </h1>
+
+<?php
+include 'conn.php'
+?>
+    <br>    
+ <p> Quantity   
+    <input type="text" size="5"> </p> 
+<p> <input type="submit" value="Enter"></p> <p style="background-color:Tomato;"> Healthy Food...</p>   
+
+  <div id ="sidebar">
+                 <center> <h4>Sign in</h4> </center>
+            </div>
+
+<img src="food1.png" width="400" height="400">
+
+</body>
+
+</html>
