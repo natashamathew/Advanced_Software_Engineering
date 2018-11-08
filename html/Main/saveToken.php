d88947b9dff17805b06744c1b30b6ed4ce031cdd
samcspohn <samuel.c.spohn@gmail.com>
2018-11-07 18:49:21 -0600

    Add files via upload

--- html/Main/saveToken.php
+++ html/Main/saveToken.php
@@ -2,21 +2,7 @@
 <html>
 <header><title>This is title</title></header>
 <body>
-Hello world
-<div> <script>
-// get the url
-var url = window.location.href;
-
-//getting the access token from url
-var access_token = url.split("#")[1].split("=")[1].split("&")[0];
-
-// get the userid
-var userId = url.split("#")[1].split("=")[2].split("&")[0];
-
-console.log(access_token);
-console.log(userId);
-</script>
-
+<div>
 <?php
 $userId = '202'
 $servername = "advsoft.codryjh8aaby.us-west-2.rds.amazonaws.com";
